1e933f2c2020c6b7c9b55acff5254285e1dfb8be
Fred Castañeda <rickzisback@gmail.com>
2022-08-04 03:43:15 +0800

    August 03, 2022

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,8 +3,10 @@ import React from 'react'
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView className="bg-yellow-400 flex-1 justify-center items-center">
-      <Text className=" text-red-700 text-3xl">Hello ano ba yan</Text>
+    <SafeAreaView className="bg-white flex-1 justify-center items-center">
+      <Text className="font-poppins-medium">Medium</Text>
+      <Text className="font-poppins-semibold"> Semi Bold</Text>
+      <Text className="font-proxima-extrabold">Proxima</Text>
     </SafeAreaView>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F7B84B",
+        secondary: ["#8B8F92", "#5E6468", "#1E1D1A", "#28ACA6", "#5ED36A", "#935CBD", "#70A3EE"]
+      },
+      fontFamily: {
+        "poppins-regular": "Poppins-Regular",
+        "poppins-medium": "Poppins-Medium",
+        "poppins-semibold": "Poppins-SemiBold",
+        "proxima-extrabold": "ProximaNovaExtraCondensed-ExtraBold"
+      }
+    },
   },
   plugins: [],
 }
